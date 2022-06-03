6ba3154cb5d58bc2e49c44f81c4ee99b38e90d78
Hrishav <hrishav.kumar@harness.io>
2022-06-03 20:46:12 +0530

    Updated current job role, added G-Tag and remove vulnerabilities in packages (#11)
    * Updated current position and remove vulnerabilities in packages
    * Added G-Tag for analytics purpose

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import Footer from "@includes/Footer";
 import Navbar from "@includes/Navbar";
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload" id="gtag">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
